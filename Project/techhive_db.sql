bd0151da123025ff513420fdd841b5eadc6f6d3f
immaculate muli <immaculatemuli25@gmail.com>
2026-06-08 08:51:05 +0300

    minor changes

--- Project/techhive_db.sql
+++ Project/techhive_db.sql
@@ -1,6 +1,3 @@
--- ============================================================
--- TechHive Database  |  BIT3208
--- ============================================================
 
 CREATE DATABASE IF NOT EXISTS techhive_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE techhive_db;
